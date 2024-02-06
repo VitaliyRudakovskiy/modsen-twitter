c1e45cc0f1622599b9b21f5aa4ef31425afafc7a
Vitaliy <vitalikrud03@example.com>
2024-02-06 15:02:01 +0300

    feat: start making auth page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
-import ButtonVariants from '@constants/buttonVariants';
-import ICONS from '@constants/icons';
 import ThemeProvider from '@providers/ThemeProvider';
-import Button from '@UI/Button';
-import Input from '@UI/Input';
-import Select from '@UI/Select';
 import ThemeToggler from '@UI/ThemeToggler';
 
 import AppContainer from './styled';
@@ -13,36 +8,6 @@ const App = () => {
     <ThemeProvider>
       <AppContainer>
         <ThemeToggler />
-        <Input placeholder='Name' />
-        <Select
-          placeholder='Day'
-          options={[
-            1219848239, 131212234234, 14234234, 1123242345, 11275089345896,
-          ]}
-          width='200px'
-        />
-        <Button width='250px' variant={ButtonVariants.primary}>
-          Primary
-        </Button>
-        <Button width='250px' variant={ButtonVariants.secondary}>
-          Secondary
-        </Button>
-        <Button
-          width='250px'
-          icon={ICONS.google}
-          variant={ButtonVariants.secondary}
-        >
-          Google
-        </Button>
-        <Button width='250px' variant={ButtonVariants.follow}>
-          Follow
-        </Button>
-        <Button width='250px' variant={ButtonVariants.primary} disabled>
-          Disabled
-        </Button>
-        <Button width='250px' variant={ButtonVariants.logout}>
-          Logout
-        </Button>
       </AppContainer>
     </ThemeProvider>
   );
---
+++ src/pages/Auth/styled.ts
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const AuthContainer = styled.section``;
+export const TwitterImage = styled.img``;
+export const TwitterLogo = styled.img``;
+export const Title = styled.h1``;
+export const Subtitle = styled.h2``;
+export const Span = styled.span``;
+export const FooterLinks = styled.div``;
+export const FooterLink = styled(Link)``;
